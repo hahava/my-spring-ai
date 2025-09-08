d4ce82362ca92ee3129550e4b5d09e7e63919141
hahava <hahava5696@gmail.com>
2025-09-08 20:13:15 +0900

    first commit

---
+++ src/main/kotlin/me/kalin/myspringai/MySpringAiApplication.kt
@@ -0,0 +1,11 @@
+package me.kalin.myspringai
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MySpringAiApplication
+
+fun main(args: Array<String>) {
+    runApplication<MySpringAiApplication>(*args)
+}
---
+++ src/test/kotlin/me/kalin/myspringai/MySpringAiApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.kalin.myspringai
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MySpringAiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
