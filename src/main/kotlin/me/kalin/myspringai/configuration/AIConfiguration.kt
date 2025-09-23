2d3b3cc5d13d9aff5b6807a2d5d546d451b77779
hahava <hahava5696@gmail.com>
2025-09-23 21:44:46 +0900

    Add advisor

--- src/main/kotlin/me/kalin/myspringai/configuration/AIConfiguration.kt
+++ src/main/kotlin/me/kalin/myspringai/configuration/AIConfiguration.kt
@@ -1,6 +1,8 @@
 package me.kalin.myspringai.configuration
 
 import org.springframework.ai.chat.client.ChatClient
+import org.springframework.ai.chat.client.advisor.SafeGuardAdvisor
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor
 import org.springframework.ai.chat.model.ChatModel
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -11,5 +13,9 @@ class AIConfiguration {
     fun chatClient(chatModel: ChatModel): ChatClient =
         ChatClient.builder(chatModel)
             .defaultSystem("모든 답변은 한국어로 하며 최대한 친절하게 그리고 출처를 함께 전달할 것")
+            .defaultAdvisors(
+                SimpleLoggerAdvisor(),
+                SafeGuardAdvisor(listOf("욕설"))
+            )
             .build()
 }
